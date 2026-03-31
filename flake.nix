8db7345415f6469f922636b7190cd5157ac88c6f
Colorman <foss@colorman.me>
2026-03-31 09:01:24 +0200

    build(nix): add nix development flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, ... }:
+      {
+        systems = lib.systems.flakeExposed;
+
+        perSystem =
+          { pkgs, ... }:
+          {
+            devShells.default = pkgs.mkShellNoCC {
+              packages = with pkgs; [
+                nil
+                fish-lsp
+              ];
+            };
+          };
+      }
+    );
+}
