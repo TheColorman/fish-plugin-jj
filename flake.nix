4c596af5690fe6271aa4c8e8bce60a48e7759da4
Colorman <foss@colorman.me>
2026-07-11 21:54:14 +0200

    flake: update to 26.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/release-26.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
